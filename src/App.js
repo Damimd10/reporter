f81417692852876cc43a4ac8b18e8854a09c9d75
Damian <damianmdst@gmail.com>
2020-05-10 21:56:04 -0300

    Add analytic to main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { pathOr, propOr } from 'ramda';
 import { useLazyQuery } from '@apollo/client';
 
+import ReactGA from 'react-ga';
+
 import Error from './components/Error';
 import Header from './components/Header';
 import Profile from './components/Profile';
@@ -13,6 +15,8 @@ import { PROFILE } from './queries';
 
 import './app.css';
 
+ReactGA.initialize(process.env.REACT_APP_ANALYTIC_TOKEN);
+
 const App = () => {
   const [showProfile, setShowProfile] = useState(true);
   const [getProfile, { data, error, loading }] = useLazyQuery(PROFILE);
@@ -23,6 +27,12 @@ const App = () => {
   const handleSearchUser = (username) => {
     getProfile({ variables: { name: username } });
   };
+
+  useEffect(() => {
+    ReactGA.event({
+      action: 'User enter to main screen',
+    });
+  }, []);
 
   useEffect(() => {
     if (data) {
